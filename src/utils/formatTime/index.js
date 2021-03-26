354d67ca5ed58dbf9cbcfd005b2509a41fbe8942
Sam Jones <samueldjones24@gmail.com>
2021-03-26 10:08:31 +0000

    Create some utils

---
+++ src/utils/formatTime/index.js
@@ -0,0 +1,19 @@
+function formatTime(s) {
+  const hours = Math.floor(s / 60 / 60);
+  const minutes = Math.floor(s / 60) - hours * 60;
+  const seconds = s % 60;
+
+  const formattedMinutes = minutes.toString() + "m ";
+  const formattedSeconds = seconds.toString() + "s";
+
+  let formattedTime;
+  if (formattedMinutes !== "0m ") {
+    formattedTime = `${formattedMinutes} ${formattedSeconds}`;
+  } else {
+    formattedTime = formattedSeconds;
+  }
+
+  return formattedTime;
+}
+
+export default formatTime;
